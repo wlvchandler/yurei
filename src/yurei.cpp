910c17a33c80523d04b5d63c64f623aa629cfca4
Adam Solloway <ajsolloway@gmail.com>
2023-07-27 20:10:55 -0400

    Add Qt dependency

--- src/yurei.cpp
+++ src/yurei.cpp
@@ -1,12 +1,17 @@
-﻿#include <j16/j16.h>
-
+﻿#include <QApplication>
+#include <mainwindow.h>
 #include <SDL.h>
+#include <j16/j16.h>
 #include <j16/runtime.h>
 
 #if defined(_WIN32) || defined(_WIN64)
 int SDL_main(int argc, char* argv[]) {
 #else
-int main() {
+int main(int argc, char *argv[]) {
 #endif
-    return 0;
+    std::cout << "HELLO" << std::endl;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
