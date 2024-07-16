048210e3224551fac29124f6fc3591a733434533
will chandler <jaskamiin@gmail.com>
2024-07-16 02:10:32 -0400

    macos/bsd don't support relative link option

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-cmake -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build --config Release
-mkdir -p bin
+PROJECTROOT=$(git rev-parse --show-toplevel)
+
+cmake -B ${PROJECTROOT}/build -DCMAKE_BUILD_TYPE=Release
+cmake --build ${PROJECTROOT}/build --config Release
+mkdir -p ${PROJECTROOT}/bin
 
 #ln -srf build/bin/oni bin/oni
 #ln -srf build/bin/yokai bin/yokai
-ln -srf build/bin/yurei bin/yurei
+ln -sf ${PROJECTROOT}/build/bin/yurei ${PROJECTROOT}/bin/yurei
