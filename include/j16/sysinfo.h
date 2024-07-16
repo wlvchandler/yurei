f986bdf91aa588559907c8c44c846072b09b7fb5
will chandler <jaskamiin@gmail.com>
2024-07-16 02:21:42 -0400

    msvc didn't like the lambda solution :)

--- include/j16/sysinfo.h
+++ include/j16/sysinfo.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstdint>
 
+#include <type_traits>
+
 enum class Endian {
     Big,
     Little
@@ -10,11 +12,21 @@ inline uint16_t swap_bytes(uint16_t x) {
     return (x << 8) | (x >> 8);
 }
 
+namespace detail {
+    template <typename T>
+    struct endianness_impl {
+        static constexpr T test_value = static_cast<T>(0x0102);
+        static constexpr bool is_little = 
+            (static_cast<std::uint8_t>(test_value) == 0x02);
+    };
+}
+
+template <typename T = std::uint16_t>
 constexpr bool isSystemLittleEndian() {
-    return []() {
-        constexpr uint16_t value = 0x0001;
-        return reinterpret_cast<const uint8_t&>(value) == 0x01;
-    }();
+    static_assert(std::is_integral<T>::value && !std::is_same<T, bool>::value,
+                  "T must be a non-bool integral type");
+    static_assert(sizeof(T) > 1, "T must be at least 16 bits");
+    return detail::endianness_impl<T>::is_little;
 }
 
 inline uint16_t correctEndianness(uint16_t value, Endian sourceEndianness) {
